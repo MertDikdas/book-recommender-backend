582bd83fda5a5fc385b9be0f5e32581e0899e80c
mertdikdas <dikdasmert@gmail.com>
2026-01-23 20:10:39 +0300

    Mapper class, maps from json to book

---
+++ scripts/mapper/json_to_book.py
@@ -0,0 +1,18 @@
+
+class JsonToBookMapper:
+    def __init__(self, json_data):
+        self.json_data = json_data
+
+    def map(self):
+        mapped_books = []
+        for item in self.json_data.get("works", []):
+            book = {
+                "work_key": item.get("key"),
+                "title": item.get("title"),
+                "first_publish_year": item.get("first_publish_year"),
+                "authors": "; ".join([author.get("name") for author in item.get("authors", []) if author.get("name")]),
+                "genre": self.json_data.get("name"),
+                "subjects": "; ".join(item.get("subject", [])),
+            }
+            mapped_books.append(book)
+        return mapped_books
