653e4cb5f8d0f9cd7dc18132a75a5b5776e17ca8
mertdikdas <dikdasmert@gmail.com>
2026-01-23 20:33:32 +0300

    Updated to constant path

--- scripts/database.py
+++ scripts/database.py
@@ -1,7 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from pathlib import Path
 
-DATABASE_URL = "sqlite:///./books.db"
+BASE_DIR = Path(__file__).resolve().parents[1]
+
+DB_PATH = BASE_DIR / "books.db"
+DATABASE_URL = f"sqlite:///{DB_PATH}"
 
 engine = create_engine(
     DATABASE_URL,
