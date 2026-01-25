4f4335ce5e25579b776654c017d038d23f4e0417
mertdikdas <dikdasmert@gmail.com>
2026-01-25 16:08:35 +0300

    Deleted old file locations

--- scripts/database/__init__.py
+++
@@ -1,4 +0,0 @@
-# scripts/database/__init__.py
-from .database import Base, SessionLocal, engine
-
-__all__ = ["Base", "SessionLocal", "engine"]
--- scripts/database/create_db.py
+++
@@ -1,6 +0,0 @@
-from scripts.database.database import Base, engine
-import scripts.models  #importing all models
-
-Base.metadata.create_all(bind=engine)
-
-print("Tables created.")
