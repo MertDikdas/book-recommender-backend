466f8ac404586b05224a54e64e75f483729c4e06
mertdikdas <dikdasmert@gmail.com>
2026-02-10 00:23:45 +0300

    Connection between fronted

--- src/api/api.py
+++ src/api/api.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.database.database import SessionLocal
 from src.api.controllers.user_controller import router as user_router
@@ -8,6 +9,18 @@ from src.api.controllers.recommendation_controller import router as recommendati
 
 
 app = FastAPI(title="Book Recommender API")
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,     
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user_router)
 app.include_router(book_router)
